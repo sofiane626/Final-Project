d2fcfb27af01305febcfc1c9120622dd6037129d
Sofiane ezz <sofianeezzahti@gmail.com>
2023-07-05 15:51:02 +0200

    premier commit, integration template

---
+++ front/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'Projet_Final/front/home.html')
+
+def product(request):
+    return render(request, 'Projet_Final/front/products-left-sidebar-2.html')
+
+def blog(request):
+    return render(request, 'Projet_Final/front/blog-5.html')
+
+def contact(request):
+    return render(request, 'Projet_Final/front/contact.html')
+
+def login(request):
+    return render(request, 'Projet_Final/front/login.html')
+
+def signup(request):
+    return render(request, 'Projet_Final/front/signup.html')
